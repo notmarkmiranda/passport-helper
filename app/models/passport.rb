deb4fb0e94125a07e28b264204a8b70cacb48ab4
Mark Miranda <notmarkmiranda@gmail.com>
2016-06-15 20:27:36 -0600

    bug squash

--- app/models/passport.rb
+++ app/models/passport.rb
@@ -1,10 +1,10 @@
 class Passport < ActiveRecord::Base
   include Formatter
 
-  has_many :groups
+  has_many :groups, dependent: :destroy
   has_many :user_passports
   has_many :users, through: :user_passports
-  has_many :specials
+  has_many :specials, dependent: :destroy
   has_many :venues, through: :specials
 
   validates :name, presence: true, uniqueness: true
