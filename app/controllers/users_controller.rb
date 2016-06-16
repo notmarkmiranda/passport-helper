2332380f3d902fd51d9ab108623a79dede7c9bb9
Mark Miranda <notmarkmiranda@gmail.com>
2016-06-15 20:41:49 -0600

    bug squash on admin show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     if @user.save
       session[:user_id] = @user.id
       flash[:success] = "Thanks for signing up!"
-      redirect_to :back
+      redirect_to user_dashboard_path
     else
       flash[:warning] = @user.errors.full_messages.join("<br />").html_safe
       redirect_to :back
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -4,6 +4,7 @@ class Venue < ActiveRecord::Base
   has_many :passports, through: :specials
   has_many :visits
   has_many :user_passports, through: :visits
+
   validates :address, presence: true
   validates :name, presence: true
   validates :name, uniqueness: { scope: :address,
